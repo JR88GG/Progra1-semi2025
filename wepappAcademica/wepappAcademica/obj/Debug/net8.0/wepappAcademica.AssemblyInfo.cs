85ce3af395e7e1817754c756e2050e0d0ea9021d
Herber Marin <juniorserpasyt@gmail.com>
2025-10-21 16:19:01 -0600

    nada relebante

--- wepappAcademica/wepappAcademica/obj/Debug/net8.0/wepappAcademica.AssemblyInfo.cs
+++ wepappAcademica/wepappAcademica/obj/Debug/net8.0/wepappAcademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wepappAcademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0ace90408016d5dbc740d375a90aa2bc59aaed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6258eb5c4b5aa4685cb994ea8a820255b237cf97")]
 [assembly: System.Reflection.AssemblyProductAttribute("wepappAcademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wepappAcademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
